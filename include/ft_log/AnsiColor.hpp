cbcc58593217360c748e9a026e29c50e060da26f
um4ss <sliziard@icloud.com>
2025-11-27 23:09:24 +0100

    feat(color): add some defines for basic ansi codes

---
+++ include/ft_log/AnsiColor.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AnsiColor.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sliziard <sliziard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/27 22:36:06 by sliziard          #+#    #+#             */
+/*   Updated: 2025/11/27 23:09:04 by sliziard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LOG_ANSI_COLOR_HPP
+# define FT_LOG_ANSI_COLOR_HPP
+
+// ============================================================================
+// ANSI color codes
+// ============================================================================
+
+# define FT_LOG_COLOR_RESET		"\033[0m"
+# define FT_LOG_COLOR_BLACK		"\033[30m"
+# define FT_LOG_COLOR_RED		"\033[31m"
+# define FT_LOG_COLOR_GREEN		"\033[32m"
+# define FT_LOG_COLOR_YELLOW	"\033[33m"
+# define FT_LOG_COLOR_BLUE		"\033[34m"
+# define FT_LOG_COLOR_MAGENTA	"\033[35m"
+# define FT_LOG_COLOR_CYAN		"\033[36m"
+# define FT_LOG_COLOR_WHITE		"\033[37m"
+
+#endif // FT_LOG_ANSI_COLOR_HPP
+
